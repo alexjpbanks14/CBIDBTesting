cf63e902b0b6d461681a712d98adf4001833de5a
alexjpbanks14 <alexjpbanks14@gmail.com>
2023-07-04 18:45:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ const connection = mysql.createConnection({
   database: 'cbidbtesting'
 })
 
+connection.connect();
+
+function createTables(){
+    connection.query('CREATE TABLE [IF NOT EXISTS] RESTRICTION_GROUPS (groupID int NOT NULL, title varchar(255), displayOrder int, PRIMARY KEY (groupID))');
+    connection.query('CREATE TABLE [IF NOT EXISTS] RESTRICTIONS (restrictionID int NOT NULL, title varchar(255), groupID int NOT NULL, active BOOLEAN, textColor varchar(10), backgroundColor varchar(10), fontWeight: varchar(30), displayOrder int, PRIMARY KEY (restrictionID), FOREIGN KEY(groupID) REFERENCES RESTRICTION_GROUPS(groupID))');
+}
+
+createTables();
+
 app.get('/', (req, res) => {
+    connection.query('SELECT * ', )
   res.send('Hello World!')
 })
 
