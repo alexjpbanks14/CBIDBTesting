8118897708905e87385fa72f47fa4464eb549a61
alexjpbanks14 <alexjpbanks14@gmail.com>
2023-07-04 17:53:57 -0400

    not

--- autoupdate.js
+++ autoupdate.js
@@ -1,14 +1,2 @@
 import AutoGitUpdate from 'auto-git-update';
 
-const config = {
-    repository: 'https://github.com/alexjpbanks14/CBIDBTesting',
-    fromReleases: false,
-    tempLocation: '/var/tmp',
-    ignoreFiles: [''],
-    executeOnComplete: '/root/CBIDBTesting/index.js',
-    exitOnComplete: true
-}
-
-const updater = new AutoGitUpdate(config);
-
-updater.autoUpdate();
