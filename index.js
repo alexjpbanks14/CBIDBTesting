d5db0e474d9a7c0ce5236ec2427c833491f3a8a9
alexjpbanks14 <alexjpbanks14@gmail.com>
2023-07-04 18:25:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import express from 'express';
+import mysql from 'mysql';
 const app = express()
 const port = 3000
+
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tokugawa',
+  database: 'cbidbtesting'
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
