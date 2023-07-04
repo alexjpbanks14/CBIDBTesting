fdecf3e6096da7464f29a5df8515680d1474af68
alexjpbanks14 <alexjpbanks14@gmail.com>
2023-07-04 19:07:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ connection.connect();
 
 function createTables(){
     connection.query('CREATE TABLE IF NOT EXISTS RESTRICTION_GROUPS(groupID int NOT NULL, title varchar(255), displayOrder int, PRIMARY KEY (groupID))');
-    connection.query('CREATE TABLE IF NOT EXISTS RESTRICTIONS(restrictionID int NOT NULL, title varchar(255), groupID int NOT NULL, active BOOLEAN, textColor varchar(10), backgroundColor varchar(10), fontWeight: varchar(30), displayOrder int, PRIMARY KEY (restrictionID), FOREIGN KEY(groupID) REFERENCES RESTRICTION_GROUPS(groupID))');
+    connection.query('CREATE TABLE IF NOT EXISTS RESTRICTIONS(restrictionID int NOT NULL, title varchar(255), groupID int NOT NULL, active BOOLEAN, textColor varchar(10), backgroundColor varchar(10), fontWeight varchar(30), displayOrder int, PRIMARY KEY (restrictionID), FOREIGN KEY(groupID) REFERENCES RESTRICTION_GROUPS(groupID))');
 }
 
 createTables();
