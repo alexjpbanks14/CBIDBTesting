a9aed85c811f7ab20be48563213d545203a627db
alexjpbanks14 <alexjpbanks14@gmail.com>
2024-04-02 02:05:37 -0400

    Update index.js

--- index.js
+++ index.js
@@ -257,12 +257,11 @@ app.post('/change_password', (req, res, next) => {
     return
   }
   bcrypt.hash(password, parseInt(config.saltRounds)).then(hash => {
-    connection.query("UPDATE " + userTableInfo.tableName + " SET passhash = ? WHERE username = ?", [hash, username], (err, results) => {
+    connection.query("UPDATE " + userTableInfo.tableName + " SET passhash = ? WHERE username = ?;", [hash, username], (err, results) => {
       if(err){
         next(err)
         return
       }
-      console.log(err);
       console.log(results)
       if(results[0].affectedRows == 0){
         res.sendStatus(400)
